9a372a186862af9753cade7e800dbe173874f73b
niao8857 <1353217510@qq.com>
2018-04-05 10:53:17 +0800

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,8 +6,14 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'sample App')</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
+@include('layouts._header')
+<div class="container">
     @yield('content')
+    @include('layouts._footer')
+</div>
+
 </body>
 </html>
